3051020074f14f74dd814cbe294e43beb43d3e7e
Jaylin Gross <grossjaylin@gmail.com>
2021-08-22 17:10:29 -0400

    commit

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const startButton = document.get
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answersButtonElement = document.getElementById('answer-buttons')
 
-function startGame() {
 
+let shuffledQuestions, currentQuestionsIndex
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+    console.log('started')
+    startButton.classList.add('hide')
+    currentQuestionsIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestions(shuffledQuestions[currentQuestionsIndex])
+}
 
+function showQuestions(question) {
+    questionElement.innerText = questions.question
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'An external JavaScript must contain the <script> tag?',
+        answers: [
+            { text: 'true', correct: false },
+            { text: 'false', correct: true },
+        ]
+    }
+]
